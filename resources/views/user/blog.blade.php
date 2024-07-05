89248767c0f6d4f3366915ef032b267f87b605fb
Andika Huga <andikahuga34@gmail.com>
2024-07-05 16:03:56 +0700

    push ke 8

--- resources/views/user/blog.blade.php
+++ resources/views/user/blog.blade.php
@@ -10,7 +10,7 @@
      <x-navbar />
 
 <body>
-
+<p></p>
     <section>
         <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-4 sm:py-12 lg:px-2 lg:py-16">
           <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
