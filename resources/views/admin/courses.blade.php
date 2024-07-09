b4df6d39e3baa35cf7e04c957d37d5ba3bbbe834
Ammar Miftahudin Anshori <ammarmiftah.06@gmail.com>
2024-07-09 17:13:14 +0700

    update view admin (user & courses)

--- resources/views/admin/courses.blade.php
+++ resources/views/admin/courses.blade.php
@@ -10,9 +10,15 @@
 <x-navbar />
 <body>
     <div class="container mx-auto mt-10">
+        <div class="container mb-5">
+            <button class="inline-block rounded bg-green-700 px-8 py-3 text-sm font-medium text-white transition hover:bg-green-900 hover:shadow-xl focus:outline-none">
+                Insert Courses
+            </button>
+        </div>
         <table class="min-w-full divide-y divide-gray-200">
             <thead>
                 <tr>
+                    <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">No</th>
                     <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Judul Materi</th>
                     <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Pemateri</th>
                     <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Link Youtube</th>
@@ -23,16 +29,22 @@
             <tbody class="bg-white divide-y divide-gray-200">
                 @foreach($courses as $course)
                 <tr>
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ $loop->iteration }}</td>
                     <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ $course->judul_course }}</td>
                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ $course->nama_pemateri }}</td>
                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ $course->link_course }}</td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ $course->text_materi }}</td>
+                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ \Illuminate\Support\Str::limit($course->text_materi, 50) }}</td>
                     <td class="text-center">
-                        <form action="{{ route('hapus', $course->id_course) }}" method="POST"">
-                            @csrf
-                            @method('DELETE')
-                            <button class="inline-block rounded bg-red-700 px-8 py-3 text-sm font-medium text-white transition hover:bg-yellow-600 hover:shadow-xl focus:outline-none">Delete</button>
-                        </form>
+                        <div class="flex justify-center space-x-2 ">
+                            <form action="#">
+                                <button class="inline-block rounded bg-blue-700 px-8 py-3 text-sm font-medium text-white transition hover:bg-blue-900 hover:shadow-xl focus:outline-none">Update</button>
+                            </form>
+                            <form action="{{ route('hapus', $course->id_course) }}" method="POST"">
+                                @csrf
+                                @method('DELETE')
+                                <button class="inline-block rounded bg-red-700 px-8 py-3 text-sm font-medium text-white transition hover:bg-red-900 hover:shadow-xl focus:outline-none">Delete</button>
+                            </form>
+                        </div>
                     </td>
                 </tr>
                 @endforeach
--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -10,6 +10,11 @@
 <x-navbar />
 <body>
     <div class="container mx-auto mt-10">
+        <div class="container mb-5">
+            <button class="inline-block rounded bg-green-700 px-8 py-3 text-sm font-medium text-white transition hover:bg-green-900 hover:shadow-xl focus:outline-none">
+                Insert User
+            </button>
+        </div>
         <table class="min-w-full divide-y divide-gray-200">
             <thead>
                 <tr>
@@ -17,6 +22,7 @@
                     <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Role</th>
                     <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
                     <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Password</th>
+                    <th class="text-center px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Aksi</th>
                 </tr>
             </thead>
             <tbody class="bg-white divide-y divide-gray-200">
@@ -26,6 +32,16 @@
                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ $user->email }}</td>
                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ $user->role }}</td>
                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ $user->password }}</td>
+                    <td class="text-center">
+                        <div class="flex justify-center space-x-2 ">
+                            <form action="#">
+                                <button class="inline-block rounded bg-blue-700 px-8 py-3 text-sm font-medium text-white transition hover:bg-blue-900 hover:shadow-xl focus:outline-none">Update</button>
+                            </form>
+                            <form action="#">
+                                <button class="inline-block rounded bg-red-700 px-8 py-3 text-sm font-medium text-white transition hover:bg-red-900 hover:shadow-xl focus:outline-none">Delete</button>
+                            </form>
+                        </div>
+                    </td>
                 </tr>
                 @endforeach
             </tbody>
